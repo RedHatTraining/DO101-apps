f6dd2f14d9e0426c0ed3d64e4bedc597cb368de4
Herve Quatremain <hquatrem@redhat.com>
2019-09-19 08:17:58 +0200

    Port changed from 3000 to 8080

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
 });
 
