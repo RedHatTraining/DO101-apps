06547e4014cec2659f4fd9f8d85b5979427f9187
Ravi Srinivasan <rsriniva@redhat.com>
2019-09-19 09:55:12 +0530

    The version app for GE 3.2

---
+++ version/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+app = express();
+
+var response;
+
+app.get('/', function (req, res) {
+
+    response = 'This is version 1 of the app.' + '\n';
+
+    //send the response to the client
+    res.send(response);
+
+});
+
+app.listen(8080, function () {
+  console.log('Server listening on port 8080...');
+});
