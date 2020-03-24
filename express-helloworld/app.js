0d9e35d41889e27e984f76fa3261da31927ef3e9
jamesdarwin4security <james.darwin@au1.ibm.com>
2020-03-24 15:08:35 +1000

    now

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
-app.get('/mars', function(req, res) { res.send('Hello Mars! \n');
+app.get('/mars', function(req, res) { res.send('Hello Mars!\n');
 });
 
 app.listen(8080, function () {
