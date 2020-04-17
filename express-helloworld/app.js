9605ade50631a1883a474be91efc4d1766f9eeb9
Adrian Hammond <AdrianHammond@users.noreply.github.com>
2020-04-17 23:27:22 +0100

    update

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/jupiter', function (req, res) {
+  res.send('Hello Jupiter!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
