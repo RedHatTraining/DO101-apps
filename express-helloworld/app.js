058aaf2f48de3df9ca2ca4014cdde3c1d5dc2dc6
sluraschi56 <sluraschi56@hotmail.com>
2020-04-19 16:20:00 -0300

    commit

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,11 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
-app.get('/seba', function (req, res) {
-  res.send('Hello Sebastian!\n');
-});
-
-
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
