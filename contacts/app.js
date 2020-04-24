67d38d3c9fffd3c48da888fa713c8e3cc4183fca
sudheernellore <sudheerknellore0107@gmail.com>
2020-04-24 11:32:57 +0100

    added comment line

--- contacts/app.js
+++ contacts/app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 
 var app = express();
-
+//
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
