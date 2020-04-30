77dbb661dfeeaebb8ae7f555f958f7c914deca42
jyotidash <59782823+jyotidash@users.noreply.github.com>
2020-04-29 20:11:25 -0400

    Update index.js

--- weather/routes/index.js
+++ weather/routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res) {
 
 router.post('/get_weather', async function (req,res) {
   let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=${UNITS}&appid=${OWM_API_KEY}`;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}ss&units=${UNITS}&appid=${OWM_API_KEY}`;
 
   try {
     let data = await fetch(url);
