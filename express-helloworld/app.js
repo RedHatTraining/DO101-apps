7bd17d99b690a3624f928f6737c1bfa4738cffeb
jlvilloria <joseluisvilloria@hotmail.com>
2020-05-01 10:32:55 +0200

    cambio2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,11 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hola World!\n');
 });
 
 app.get('/mars', function(req, res) {
-  res.send('Hello Mars!\n');
+  res.send('HolaMars!\n');
 });
 
 app.listen(8080, function () {
