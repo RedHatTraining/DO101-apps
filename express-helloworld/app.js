d507a6d654c68ca109089a1761eabee9b56addca
superlinuxex <superlinuxec@gmail.com>
2020-05-03 19:36:55 +0200

    se añade endpoint
    se añade endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,6 +4,9 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
+app.get('/Alex', function(req, res) {
+  res.send('Hello ALex!\n');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
