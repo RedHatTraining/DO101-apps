cf63c4d059de476f7826b0569a95ae94918566e3
christiano <christiano.rios@gmail.com>
2020-05-02 21:35:03 -0300

    add /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/mars', function (req, res) {
+  res.send('Hello Mars!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
