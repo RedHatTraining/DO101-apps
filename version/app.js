eaf06faf10b2db908dd519e8c51b899d5c9e8baf
randifilan <randimaskil9@gmail.com>
2020-05-04 21:15:23 +0700

    Version 4

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 3 of the app by Randy Filan' + '\n';
+    response = 'This is version 4 of the app by Randy Filan' + '\n';
 
     //send the response to the client
     res.send(response);
