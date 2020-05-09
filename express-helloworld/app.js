30ad1b9f5d32ff83bed993f7070390010453d7a1
Vishal Mishra <vishalmishra84@gmail.com>
2020-05-09 05:59:36 +0400

    Added mars endpoint to app

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/mars', function (req, res) {
+  res.send('Hello Mars!\n');
+});
+
+app.get('/vm', function (req, res) {
+  res.send('Hello Vishak Mishra!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
