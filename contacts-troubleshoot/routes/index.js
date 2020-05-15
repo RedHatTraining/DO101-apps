7a3fda245bd3061b4696346c8eaa7bff9c91e0ec
pingumanbr <manoel.dantas@gmail.com>
2020-05-15 11:36:42 -0300

    Mudanca codigo

--- contacts-troubleshoot/routes/index.js
+++ contacts-troubleshoot/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { pgconn } = require('../db/config')
+const pgconn = require('../db/config')
 
 function get_contacts(res)
 {
