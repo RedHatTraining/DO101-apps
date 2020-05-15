7085631631143e15d5e6a91c74c2f09c5f7c3462
pingumanbr <manoel.dantas@gmail.com>
2020-05-15 12:07:55 -0300

    Erro db

--- contacts-troubleshoot/db/config.js
+++ contacts-troubleshoot/db/config.js
@@ -9,7 +9,7 @@ var db_name = process.env["database-name"];
 
 const DB_CONFIG = util.format('postgresql://%s:%s@%s:%s/%s', db_user, db_pass, process.env.POSTGRESQL_SERVICE_HOST, process.env.POSTGRESQL_SERVICE_PORT, db_name);
 
-const { Pool } = require('pg');
+const Pool  = require('pg');
 
 const pgconn = new Pool({
     connectionString: DB_CONFIG,
