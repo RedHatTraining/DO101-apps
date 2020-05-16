932dbdd774d6d772f99ec0c9f8b09077b8c1ebbf
Ryan Nix <r-nix@northwestern.edu>
2020-05-16 10:27:31 -0500

    Initial upload

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,9 +1,12 @@
-#Adding code
 var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello OpenShift!\n');
+});
+
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars!\n');
 });
 
 app.listen(8080, function () {
