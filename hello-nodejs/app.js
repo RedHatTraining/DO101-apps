4790c6a2e6618bd330e33bed759b88df70711fec
carlodrs <carlosrenato.s@gmail.com>
2020-05-25 20:31:50 -0400

    commit code red hat training

--- hello-nodejs/app.js
+++ hello-nodejs/app.js
@@ -1,2 +1,21 @@
-console.log("Hello World!\n");
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  console.log('estou no consolse')
+  res.send('ola mundo');
+});
+
+
+app.get('/mars', function (req, res) {
+  console.log('estou no consolse')
+  res.send('ola marte');
+});
+
+
+
+app.listen(8000, function () {
+  console.log('escrevendo na porta 8000!');
+});
 
