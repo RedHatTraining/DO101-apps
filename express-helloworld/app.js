5a3fc8d30509772536e6240d748506c848b34494
pabloavallejo <37087606+pabloavallejo@users.noreply.github.com>
2020-06-02 22:06:09 -0300

    Solar

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World, Hello Sun and Hi Sollar System!\n');
+    res.send('Hello World, Hello Sun and Hi Solar System!\n');
 });
 
 app.get('/mars', function(req, res) {
