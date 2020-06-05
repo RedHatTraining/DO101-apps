ffd6ff8e5063687d41941b7d89ea0ba18dc7af37
Robert Diaz <rad8329@gmail.com>
2020-06-05 13:44:26 -0500

    ES6 refactor

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,15 +1,8 @@
-var express = require('express');
-app = express();
+const express = require('express')
+const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!\n');
-});
+app.get('/', (req, res) => res.send('Hello World!\n'));
 
-app.get('/mars', function (req, res) {
-  res.send('Hello Mars!\n');
-});
+app.get('/mars', (req, res) => res.send('Hello Mars!\n'))
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
-
+app.listen(8080, () => console.log('Example app listening on port 8080!'))
