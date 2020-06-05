ced06b26ee0f2f260c35e12e08f5c940c8b1d9a8
Shiraga <matheus.s.rasera@gmail.com>
2020-06-05 11:42:24 -0300

    update app to v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,11 +1,18 @@
-var express = require('express');
-app = express();
+// var express = require('express');
+// app = express();
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!\n');
+// });
+
+// app.listen(8080, function () {
+//   console.log('Example app listening on port 8080!');
+// });
+
+var response;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
-});
+    response = 'This is version 2 of the app.' + '\n';
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
-
+    //send the response to the client
+    res.send(response);
