d28907354e0feb2b1ca75ce105740d735932a4b6
lletsica <lletsica@gmail.com>
2020-06-11 16:29:54 -0400

    add /mars endpoint to the application

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,7 +5,11 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.get('/mars', function (req, res) {
+  res.send('Hello Mars!\n');
+});
+
+app.listen(8085, function () {
+  console.log('Example app listening on port 8085!');
 });
 
