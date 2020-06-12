9529a0041efbd248f077c86070ff091c689109d1
carlodrs <carlosrenato.s@gmail.com>
2020-06-12 07:38:18 -0400

    novas modificacoes

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,11 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World = Carlos mundo!\n');
+  res.send('Hello World = Carlos mundo -B!\n');
 });
 
 app.get('/mars', function (req, res) {
-  res.send('Hello MARS! = Carlos Marte\n');
+  res.send('Hello MARS! = Carlos Marte - b\n');
 });
 
 app.listen(8080, function () {
