8c6b4ebe8ebf48320887822dbfddcfe8459f85bb
Robert Diaz <rad8329@gmail.com>
2020-06-15 10:12:39 -0500

    Mercury endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -7,4 +7,6 @@ app.get('/mars', (req, res) => res.send('Hello Mars!\n'))
 
 app.get('/venus', (req, res) => res.send('Hello Venus!\n'))
 
+app.get('/mercury', (req, res) => res.send('Hello Mercury!\n'))
+
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
