9df32c95eb25d19cc7213eb5f2e111386567f79a
Dana <dana.reale@outlook.com>
2020-06-21 17:07:32 -0500

    Display msg when visiting the /mars endpoint.

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,7 +5,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+// Display different message when someone visits /mars endpoint.
+app.get('/mars', function(req, res) {
+  res.send('Hello Mars!\n');
+});
+
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app listening on port 8080, to visit open http://localhost:8080!');
 });
 
