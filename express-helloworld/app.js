075eb4b1089ac16e2619b3ceb556a365699fc15e
Kevin C Myers <kevin.myers@redhat.com>
2020-06-30 08:04:44 -0400

    Added moon and adjusted welcome message

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,11 +2,15 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World!  Welcome to DO101\n');
 });
 
 app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
+});
+
+app.get('/moon', function (req, res) {
+  res.send('Hello Apollo!\n');
 });
 
 app.listen(8080, function () {
