45f99ef07559ad135f3a9630707aea792771e466
lynn99zhang <67778765+lynn99zhang@users.noreply.github.com>
2020-07-04 16:19:57 -0700

    /adding Hello Mars

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/mars', function (req, res) {
+  res.send('Hello Mars! Lynn is greeting to you! \n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
