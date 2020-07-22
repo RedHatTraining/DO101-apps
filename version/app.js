c909827cf4d9352ffd7f5d6915b09a479950b9b8
lynn99zhang <67778765+lynn99zhang@users.noreply.github.com>
2020-07-21 22:49:11 -0700

    version 1_lin

--- version/app.js
+++ version/app.js
@@ -11,6 +11,14 @@ app.get('/', function (req, res) {
     res.send(response);
 
 });
+app.get('/mars', function (req, res) {
+
+  response = 'This is version 1_lin of the app. Hello Mars!' + '\n';
+
+  //send the response to the client
+  res.send(response);
+
+});
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
