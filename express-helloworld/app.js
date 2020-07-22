463cfee7d341f3130b7538e940fdfa21a92bbbf4
SSJNM <nishant010singh@gmail.com>
2020-07-22 11:51:15 +0530

    change1 by developer

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
-app.get('/', function (req, res) {
+app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
