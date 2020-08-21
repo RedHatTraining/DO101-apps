1aaa1ffba2107f3ac3e90e5e9686c0d97549fa42
Danken01 <rinconsegovia@gmail.com>
2020-08-20 19:28:47 -0500

    add /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('The cake is a lie on Earth!\n');
+});
+
+app.get('/mars', function (req, res) {
+  res.send('The cake is a lie on Mars too!\n');
 });
 
 app.listen(8080, function () {
