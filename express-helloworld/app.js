6e4be6f05231ff205f054139759daedda8c18cde
craig mckeeman <craigmckeeman@googlemail.com>
2020-10-29 22:05:35 +0000

    Update manual

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Universe!\n');
+  res.send('Hello Universe Manual!\n');
 });
 
 app.listen(8080, function () {
