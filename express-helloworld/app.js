48052187598549db56ad7d7ada3cf3da4cbe9a4b
craig mckeeman <craigmckeeman@googlemail.com>
2020-10-29 22:28:43 +0000

    automate changes

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Universe Manual!\n');
+  res.send('Hello Universe Auto!\n');
 });
 
 app.listen(8080, function () {
