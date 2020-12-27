a4360e4509dcf4997dcbf24502fc6820c0c38628
slopeup <slopeup.0821@gmail.com>
2020-12-27 19:03:15 +0900

    test

--- contacts/app.js
+++ contacts/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var sakaue=require('AAA');
 var indexRouter = require('./routes/index');
 
 var app = express();
