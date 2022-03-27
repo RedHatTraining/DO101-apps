399d2733704e9bbf630866c4720d254778f7fd09
Tiffany <29214176+tharrell4@users.noreply.github.com>
2022-03-26 21:11:39 -0400

    commit

--- weather/app.js
+++ weather/app.js
@@ -1,3 +1,5 @@
+// main app
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +10,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-// view engine setup
+// view engine setup 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -20,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler  
 app.use(function(req, res, next) {
   next(createError(404));
 });
