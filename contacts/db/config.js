f478ee2914901291e9479654d990bfc608e2eaf5
Warckor <mik.el.loco@gmail.com>
2022-06-21 10:59:48 +0200

    again

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contactsdb';
+const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@contactsdb:5432/contactsdb';
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
