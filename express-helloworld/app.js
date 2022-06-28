6be02fefc24fc169fa764ec336fdd4a9b363a512
Dianamarlen12 <diana.meneses@ids.com.mx>
2022-06-27 20:19:52 -0500

    add /mars endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+/**Agregar ruta para poner el nombre */
 app.get('/mars', function(req, res) {
   res.send('Hello Mars!\n');
   });
