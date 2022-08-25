704b57b78bfde8a5ab772579a1d58b2f0dcf66da
krishnamurthypradeep <pradeeepmurthy@gmail.com>
2022-08-25 17:30:33 +0530

    changed to version2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Version1!\n '+ new Date());
+  res.send('Version2!\n '+ new Date());
 });
 
 app.listen(8080, function () {
