5487c8e06a83dbe533ff9cdd3ed421b53b1e2772
Prachiti <ptalgulk@redhat.com>
2023-09-12 03:24:36 -0400

    contact commit

--- contacts/db/config.js
+++ contacts/db/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contacts';
+const DB_CONFIG = process.env.DB_CONFIG || 'postgresql://contacts:contacts@localhost:5432/contactsdb';
 const { Pool } = require('pg');
 
 const pgconn = new Pool({
