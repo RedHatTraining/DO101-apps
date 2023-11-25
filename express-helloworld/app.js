26472b7abae6c3554cd09e2d2c82259205553366
Snehakethireddi <snehakethireddi04@gmail.com>
2023-11-25 12:09:57 +0530

    add /krypton endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -4,6 +4,9 @@ app = express();
 app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
+app.get('/krypton', function (req, res) {
+  res.send('yeahyeahhh!\n');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
