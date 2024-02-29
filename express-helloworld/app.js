431575ed2936073b2b237b0c12df6f0255506a37
Yariv Rachmani <yrachman@redhat.com>
2024-02-29 16:45:38 +0200

    Add hello mercury

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function(req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/mercury', function(req, res) {
+  res.send('Hello Mercury!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
